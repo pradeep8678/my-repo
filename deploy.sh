dc4a6a774742fc41f5fa91560c2003c726d4d208
Pradip Singh <pradeepsinghania906@gmail.com>
2025-09-22 20:50:44 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,35 +1,30 @@
 #!/bin/bash
 set -e
 
-# Commit SHA passed from Cloud Build
-COMMIT_SHA=$1
-
-# Make a safe template name (start with letter, use only letters, numbers, hyphens)
-SAFE_SHA=$(echo $COMMIT_SHA | tr '[:upper:]' '[:lower:]' | sed 's/[^a-z0-9]/-/g')
-TEMPLATE_NAME="my-app-template-${SAFE_SHA}"
-
 # Variables
-MIG_NAME=${_MIG_NAME}
-ZONE=${_ZONE}
-IMAGE="asia-south1-docker.pkg.dev/psyched-option-421700/artifact-repo/simple-web-app:${COMMIT_SHA}"
+PROJECT_ID="psyched-option-421700"
+ZONE="asia-south1-a"
+MIG_NAME="my-app-mig"
+IMAGE="asia-south1-docker.pkg.dev/$PROJECT_ID/artifact-repo/simple-web-app:$COMMIT_SHA"
+TEMPLATE_NAME="my-app-template-$COMMIT_SHA"
 
-echo "Deploying version with commit SHA: $COMMIT_SHA"
-echo "Using instance template: $TEMPLATE_NAME"
-
-# 1. Create a new instance template with the new Docker image
+# 1. Create new instance template
+echo "Creating instance template: $TEMPLATE_NAME"
 gcloud compute instance-templates create $TEMPLATE_NAME \
+    --project=$PROJECT_ID \
     --machine-type=e2-small \
-    --image-family=cos-stable \
-    --image-project=cos-cloud \
-    --boot-disk-size=20GB \
+    --network=default \
     --metadata=startup-script="#!/bin/bash
-docker-credential-gcr configure-docker
-docker pull ${IMAGE}
-docker run -d -p 8080:8080 ${IMAGE}"
-
-# 2. Update the MIG to use the new template
-gcloud compute instance-groups managed rolling-action replace $MIG_NAME \
-    --template=$TEMPLATE_NAME \
+        docker-credential-gcr configure-docker
+        docker pull $IMAGE
+        docker run -d -p 8080:8080 $IMAGE" \
+    --tags=http-server \
     --zone=$ZONE
 
-echo "Deployment triggered successfully."
+# 2. Update MIG to use new template
+echo "Updating MIG: $MIG_NAME to use template $TEMPLATE_NAME"
+gcloud compute instance-groups managed rolling-action replace $MIG_NAME \
+    --zone=$ZONE \
+    --version=template=$TEMPLATE_NAME
+
+echo "Deployment complete!"

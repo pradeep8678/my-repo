015efa3fb669061bbf3c3719e4a049ccaaf194f6
Pradip Singh <pradeepsinghania906@gmail.com>
2025-09-22 20:56:26 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,29 +2,32 @@
 set -e
 
 # Variables
-PROJECT_ID="psyched-option-421700"
-ZONE="asia-south1-a"
-MIG_NAME="my-app-mig"
-IMAGE="asia-south1-docker.pkg.dev/$PROJECT_ID/artifact-repo/simple-web-app:$COMMIT_SHA"
-TEMPLATE_NAME="my-app-template-$COMMIT_SHA"
+PROJECT_ID="${_PROJECT_ID}"
+ZONE="${_ZONE}"
+MIG_NAME="${_MIG_NAME}"
+IMAGE="asia-south1-docker.pkg.dev/${PROJECT_ID}/artifact-repo/simple-web-app:${COMMIT_SHA}"
+TEMPLATE_NAME="my-app-template-${COMMIT_SHA}"
 
-# 1. Create new instance template
+if [ -z "$COMMIT_SHA" ]; then
+  echo "Error: COMMIT_SHA is not set!"
+  exit 1
+fi
+
 echo "Creating instance template: $TEMPLATE_NAME"
-gcloud compute instance-templates create $TEMPLATE_NAME \
-    --project=$PROJECT_ID \
+
+gcloud compute instance-templates create "$TEMPLATE_NAME" \
+    --project="$PROJECT_ID" \
     --machine-type=e2-small \
     --network=default \
     --metadata=startup-script="#!/bin/bash
-        docker-credential-gcr configure-docker
+        apt-get update && apt-get install -y docker.io
         docker pull $IMAGE
         docker run -d -p 8080:8080 $IMAGE" \
-    --tags=http-server \
-    --zone=$ZONE
+    --tags=http-server
 
-# 2. Update MIG to use new template
-echo "Updating MIG: $MIG_NAME to use template $TEMPLATE_NAME"
-gcloud compute instance-groups managed rolling-action replace $MIG_NAME \
-    --zone=$ZONE \
-    --version=template=$TEMPLATE_NAME
+echo "Updating managed instance group: $MIG_NAME"
+gcloud compute instance-groups managed rolling-action replace "$MIG_NAME" \
+    --zone="$ZONE" \
+    --version=template="$TEMPLATE_NAME"
 
 echo "Deployment complete!"

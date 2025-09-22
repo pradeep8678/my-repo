c6505335e1dc1caad0622089664bc68f599652e1
Pradip Singh <pradeepsinghania906@gmail.com>
2025-09-22 20:45:57 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,13 +4,17 @@ set -e
 # Commit SHA passed from Cloud Build
 COMMIT_SHA=$1
 
+# Make a safe template name (start with letter, use only letters, numbers, hyphens)
+SAFE_SHA=$(echo $COMMIT_SHA | tr '[:upper:]' '[:lower:]' | sed 's/[^a-z0-9]/-/g')
+TEMPLATE_NAME="my-app-template-${SAFE_SHA}"
+
 # Variables
 MIG_NAME=${_MIG_NAME}
-TEMPLATE_NAME="${_TEMPLATE_NAME_PREFIX}-${COMMIT_SHA}"
 ZONE=${_ZONE}
 IMAGE="asia-south1-docker.pkg.dev/psyched-option-421700/artifact-repo/simple-web-app:${COMMIT_SHA}"
 
 echo "Deploying version with commit SHA: $COMMIT_SHA"
+echo "Using instance template: $TEMPLATE_NAME"
 
 # 1. Create a new instance template with the new Docker image
 gcloud compute instance-templates create $TEMPLATE_NAME \

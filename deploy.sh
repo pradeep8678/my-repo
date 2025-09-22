470d7544eb3c62ac7c12c09d625e5a805a3894e8
Pradip Singh <pradeepsinghania906@gmail.com>
2025-09-22 20:41:51 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,40 +1,31 @@
 #!/bin/bash
 set -e
 
-PROJECT=$1
-ZONE=$2
-LIVE_MIG=$3
-IDLE_MIG=$4
-BACKEND=$5
-COMMIT_SHA=$6
+# Commit SHA passed from Cloud Build
+COMMIT_SHA=$1
 
-TEMPLATE_NAME="my-app-template-$COMMIT_SHA"
+# Variables
+MIG_NAME=${_MIG_NAME}
+TEMPLATE_NAME="${_TEMPLATE_NAME_PREFIX}-${COMMIT_SHA}"
+ZONE=${_ZONE}
+IMAGE="asia-south1-docker.pkg.dev/psyched-option-421700/artifact-repo/simple-web-app:${COMMIT_SHA}"
 
-echo "Creating instance template $TEMPLATE_NAME..."
+echo "Deploying version with commit SHA: $COMMIT_SHA"
+
+# 1. Create a new instance template with the new Docker image
 gcloud compute instance-templates create $TEMPLATE_NAME \
-  --project=$PROJECT \
-  --machine-type=e2-small \
-  --image-family=cos-stable \
-  --image-project=cos-cloud \
-  --boot-disk-size=20GB \
-  --metadata=startup-script='#!/bin/bash
-docker pull asia-south1-docker.pkg.dev/'$PROJECT'/artifact-repo/simple-web-app:'$COMMIT_SHA'
-docker run -d -p 8080:8080 asia-south1-docker.pkg.dev/'$PROJECT'/artifact-repo/simple-web-app:'$COMMIT_SHA
+    --machine-type=e2-small \
+    --image-family=cos-stable \
+    --image-project=cos-cloud \
+    --boot-disk-size=20GB \
+    --metadata=startup-script="#!/bin/bash
+docker-credential-gcr configure-docker
+docker pull ${IMAGE}
+docker run -d -p 8080:8080 ${IMAGE}"
 
-echo "Rolling update on idle MIG $IDLE_MIG..."
-gcloud compute instance-groups managed rolling-action replace $IDLE_MIG \
-  --template=$TEMPLATE_NAME \
-  --zone=$ZONE
+# 2. Update the MIG to use the new template
+gcloud compute instance-groups managed rolling-action replace $MIG_NAME \
+    --template=$TEMPLATE_NAME \
+    --zone=$ZONE
 
-echo "Updating load balancer backend..."
-gcloud compute backend-services remove-backend $BACKEND \
-  --instance-group=$LIVE_MIG \
-  --instance-group-zone=$ZONE \
-  --global
-
-gcloud compute backend-services add-backend $BACKEND \
-  --instance-group=$IDLE_MIG \
-  --instance-group-zone=$ZONE \
-  --global
-
-echo "Deployment complete. Switch complete!"
+echo "Deployment triggered successfully."

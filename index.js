a5e88a492fd51e37f50013cf70d2b06e0372f0de
Pradip Singh <pradeepsinghania906@gmail.com>
2025-09-22 10:54:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
-  res.send('Namaste India 2');
+  res.send('Namaste ');
 });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
 
 

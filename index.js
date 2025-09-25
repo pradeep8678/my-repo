781d7dbf68546eb5bcdb214240f3711cd11007bc
Pradip Singh <pradeepsinghania906@gmail.com>
2025-09-25 10:53:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,4 @@ app.get('/', (req, res) => {
   res.send('Hellooo Ratannnn');
 });
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
